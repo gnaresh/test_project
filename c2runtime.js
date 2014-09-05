24fbcbcab1cbcb3fcc321ff8831ad465ee2503fb
gnaresh <naresh.itian@gmail.com>
2014-09-05 18:05:25 +0530

    hh

--- c2runtime.js
+++ c2runtime.js
@@ -13105,18 +13105,6 @@ cr.plugins_.PhonegapFB = function(runtime)
 		if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
             if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
             if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
-            FB.Event.subscribe('auth.login', function(response) {
-                               alert('auth.login event');
-                               });
-            FB.Event.subscribe('auth.logout', function(response) {
-                               alert('auth.logout event');
-                               });
-            FB.Event.subscribe('auth.sessionChange', function(response) {
-                               alert('auth.sessionChange event');
-                               });
-            FB.Event.subscribe('auth.statusChange', function(response) {
-                               alert('auth.statusChange event');
-                               });
 	};
 	pluginProto.Instance = function(type)
 	{
@@ -13153,6 +13141,9 @@ cr.plugins_.PhonegapFB = function(runtime)
 	function Acts() {};
 Acts.prototype.FBPGLogin = function ()
 	{
+            FB.Event.subscribe('auth.login', function(response) {
+                               alert('auth.login event');
+                               });
 		FB.login(
                          function(response) {
                          if (response.session) {
